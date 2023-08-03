17762bcb4d3c99347cfcd0476b16fc410894be3c
Илья <2005ilya5002@gmail.com>
2023-08-03 17:00:48 +0700

    homework lesson 7

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField('Заголовок', max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Отьметье, если торг уместен')
+    created_at = models.DateTimeField('Дата и время создания', auto_now_add=True)
+    updated_at = models.DateTimeField('Дата и время изменения', auto_now=True)
+
+    def __str__(self):
+        return f"<Advertisement: Advertisement(id={self.pk}, title={self.title}, price={self.price:.2f})>"
+
+    class Meta:
+        db_table = 'advertisements'
